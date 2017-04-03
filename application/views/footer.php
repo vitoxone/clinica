d80000a235e266b9ab3f9603c5f0e43dfcae3344
victor silva <vitox.one@gmail.com>
2017-04-03 03:20:33 -0300

    se agrega desarrollado por itsia

--- application/views/footer.php
+++ application/views/footer.php
@@ -5,7 +5,7 @@
     <div class="row">
       <div class="col-md-12">
             <!-- Copyright info -->
-            <p class="copy">Copyright &copy; 2017 | <a href="#">Convatec</a> </p>
+            <p class="copy">Copyright &copy; 2017 | <a href="#">Convatec</a> </p> Desarrollado por <a target="_blank" href="http://itsia.cl/"><img border="0" alt="W3Schools" src="<?php echo base_url(); ?>assets/img/itsia.png" width="55" height="20"></a>
       </div>
     </div>
   </div>
